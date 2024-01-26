943641e2fdad9a511401138135f51c50a8840b20
Shashant <shashantchaudhary999@gmail.com>
2024-01-26 17:17:10 +0530

    26-01-2024

---
+++ FoodDeliveryDAL/Migrations/202401261039297_addOrderStatusToOrderDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace FoodDeliveryDAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addOrderStatusToOrderDetails : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OrderDetails", "OrderStatus", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OrderDetails", "OrderStatus");
+        }
+    }
+}
